eecea64d771d105187848d4e6ee9ee310f4ccf22
RosarioGle <8824@holbertonstudents.com>
2024-12-12 14:20:45 +0000

    creation du fichier 1-insertion_sort_list.c et oublie d'une fonction dans le fichier 1-main.h

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+ * swap_nodes - Swap two nodes in the doubly-linked list
+ *
+ * @h: the pointer to head of the doubly-linked list
+ * @n1: the pointer to first node to swap
+ * @n2: the second node to swap
+ */
+
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending
+ * order using the Insertion sort algorithm
+ *
+ * @list: the doubly linked list from an array of integers
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *iter, *insert, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	for (iter = (*list)->next; iter != NULL; iter = temp)
+	{
+		temp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
+		{
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)*list);
+		}
+	}
+}
